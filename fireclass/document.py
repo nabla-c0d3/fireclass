3dc296f4254b23e57e9b8cafda9f662ae100f28e
Alban Diquet <nabla.c0d3@gmail.com>
2019-06-28 21:25:21 -0700

    Move classes around

--- fireclass/document.py
+++ fireclass/document.py
@@ -29,29 +29,6 @@ def _get_firestore_client() -> firestore.Client:
     return _firestore_client
 
 
-_DocumentClsTypeVar = TypeVar('_DocumentClsTypeVar', bound="Document")
-
-
-class _DocumentQuery:
-
-    def __init__(self, document_cls: Type[_DocumentClsTypeVar], firestore_query: Query) -> None:
-        self._document_cls = document_cls
-        self._firestore_query = firestore_query
-
-    def limit(self, count: int) -> '_DocumentQuery':
-        new_query = self._firestore_query.limit(count)
-        return _DocumentQuery(self._document_cls, new_query)
-
-    def get(self, transaction: Optional[Transaction] = None) -> Iterator[_DocumentClsTypeVar]:
-        for firestore_document in self._firestore_query.get(transaction):
-            document = self._document_cls(**firestore_document.to_dict())
-            document._id = firestore_document.id
-            yield document
-
-
-FirestoreOperator = Literal["<", "<=", "==", ">=", ">", "array_contains"]
-
-
 class DocumentNotFound(Exception):
     pass
 
@@ -64,7 +41,27 @@ class DocumentAlreadyExistsInDatabase(Exception):
     pass
 
 
-_DocumentTypeVar = TypeVar('_DocumentTypeVar', bound="Document")
+_DocumentSubclassTypeVar = TypeVar('_DocumentSubclassTypeVar', bound="Document")
+
+
+class _DocumentQuery:
+
+    def __init__(self, document_cls: Type[_DocumentSubclassTypeVar], firestore_query: Query) -> None:
+        self._document_cls = document_cls
+        self._firestore_query = firestore_query
+
+    def limit(self, count: int) -> '_DocumentQuery':
+        new_query = self._firestore_query.limit(count)
+        return _DocumentQuery(self._document_cls, new_query)
+
+    def get(self, transaction: Optional[Transaction] = None) -> Iterator[_DocumentSubclassTypeVar]:
+        for firestore_document in self._firestore_query.get(transaction):
+            document = self._document_cls(**firestore_document.to_dict())
+            document._id = firestore_document.id
+            yield document
+
+
+FirestoreOperator = Literal["<", "<=", "==", ">=", ">", "array_contains"]
 
 
 @dataclass
@@ -99,11 +96,11 @@ class Document:
         return self.delete_document(self.id)
 
     @classmethod
-    def _collection(cls: Type[_DocumentTypeVar]) -> CollectionReference:
+    def _collection(cls: Type[_DocumentSubclassTypeVar]) -> CollectionReference:
         return _get_firestore_client().collection(cls.__name__)
 
     @classmethod
-    def get_document(cls: Type[_DocumentTypeVar], document_id: str) -> _DocumentTypeVar:
+    def get_document(cls: Type[_DocumentSubclassTypeVar], document_id: str) -> _DocumentSubclassTypeVar:
         firestore_document = cls._collection().document(document_id).get()
         if not firestore_document or not firestore_document.exists:
             raise DocumentNotFound()
@@ -112,11 +109,11 @@ class Document:
         return document
 
     @classmethod
-    def delete_document(cls: Type[_DocumentTypeVar], document_id: str) -> Timestamp:
+    def delete_document(cls: Type[_DocumentSubclassTypeVar], document_id: str) -> Timestamp:
         return cls._collection().document(document_id).delete()
 
     @classmethod
-    def get(cls: Type[_DocumentTypeVar]) -> Iterator[_DocumentTypeVar]:
+    def get(cls: Type[_DocumentSubclassTypeVar]) -> Iterator[_DocumentSubclassTypeVar]:
         for firestore_document in cls._collection().get():
             document = cls(**firestore_document.to_dict())
             document._id = firestore_document.id
@@ -124,7 +121,7 @@ class Document:
 
     @classmethod
     def where(
-            cls: Type[_DocumentTypeVar],
+            cls: Type[_DocumentSubclassTypeVar],
             field_path: str,
             op_string: FirestoreOperator,
             value: Any
